fdcd442e8de4c5dc589f4d4db56ec3e0b24def3f
Kailee <kaileeagray@gmail.com>
2018-02-22 11:26:15 -0600

    trying to delay load

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # gray_meek_two
 
+use flickity for image carousel on click
+
 [Following CSS Grid Tutorial](https://youtu.be/MeXbMmdn9f8 "How to create website layouts using CSS grid Learn HTML and CSS HTML tutorial")
 
 [Second CSS Grid Tutorial](https://youtu.be/jV8B24rSN5o "CSS Grid Layout Crash Course")
--- js/app.js
+++ js/app.js
@@ -1,9 +1,7 @@
-$(".grid-photo").imagesLoaded( function() {
+$(".grid-photo, .bg-img").imagesLoaded({ background: true }, function() {
   $(".grid-photo").masonry({
     itemSelector: ".grid-item"
   });
-});
-
-$('#showcase').imagesLoaded( { background: true }, function() {
-  $(document).load();
+  alert('test');
+  
 });
